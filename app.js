125991aee49b091dc4e62b48656de5b8ab2e73ab
evandro1760 <evandro.1760@gmail.com>
2018-08-23 14:49:18 -0300

    eita

---
+++ app.js
@@ -0,0 +1,79 @@
+'use strict';
+
+const app = require('express')();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', (req, res) => {
+  res.status(200).send('Hello, world!');
+});
+
+app.post('/apiai', function(req, res){
+    let mensagem = req.body;
+    let session_id = req.body.session_id;
+    let resposta = {
+      "fulfillmentText": "EITA, eu vim do WeebHook",
+      "fulfillmentMessages": [
+        // {
+        //   "card": {
+        //     "title": "card title",
+        //     "subtitle": "card text",
+        //     "imageUri": "https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png",
+        //     "buttons": [
+        //       {
+        //         "text": "button text",
+        //         "postback": "https://assistant.google.com/"
+        //       }
+        //     ]
+        //   }
+        // }
+      ],
+      "source": "example.com",
+      "payload": {
+        "google": {
+          "expectUserResponse": true,
+          "richResponse": {
+            "items": [
+              {
+                "simpleResponse": {
+                  "textToSpeech": "this is a simple response"
+                }
+              }
+            ]
+          }
+        },
+        "facebook": {
+          "text": "EITA, eu vim do WeebHook, agora to no Facebook!"
+
+        },
+        "slack": {
+          "text": "EITA, eu vim do WeebHook, agora to no Slack.",
+        }
+      },
+      "outputContexts": [
+        {
+          "name": "projects/responsive-edge-212711/agent/sessions/"+session_id+"/contexts/Empresas-no-followup",
+          "lifespanCount": 5,
+          "parameters": {
+          }
+        }],
+      "followupEventInput":  {
+        "name": "WELCOME",
+        "languageCode": "pt-BR",
+        "parameters": {
+        }
+    }}
+
+    res.status(200).json(resposta);
+    
+});
+
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+  console.log('Press Ctrl+C to quit.');
+});
